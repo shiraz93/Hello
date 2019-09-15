1773542550da17e5fa1fa4e3c49d8689157adb6a
Shiraz <shiraz007@list.ru>
2019-09-15 18:21:50 +0400

    first commit

---
+++ src/com/company/Lesson1a.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Lesson1a {
+    void Iseven(int numb) {
+        if (numb % 2 != 0) {
+            System.out.println("not even");
+        } else {
+            System.out.println("even");
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("hello");
+        Lesson1a a = new Lesson1a();
+        a.Iseven(scanner.nextInt());
     }
 }
