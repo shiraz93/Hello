383760d5a587e8092759a935c43d62f76728b912
Shiraz <shiraz007@list.ru>
2019-09-17 13:33:52 +0400

    orginal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner=new Scanner(System.in);
-        Lesson1e e=new Lesson1e();
-        e.Checknumber(scanner.nextInt());
+        System.out.println("hello");
+
     }
 }
