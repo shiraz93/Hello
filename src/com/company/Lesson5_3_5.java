f0ab111349e8f97e28117c33405977dde6a96003
Shiraz <shiraz007@list.ru>
2019-09-18 13:23:33 +0400

    lesson5.3.5versia1

---
+++ src/com/company/Lesson5_3_5.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Lesson5_3_5 {
+    /**
+     * print  it is possible to get the second from the letters of the first
+     *
+     * @param word1
+     * @param word2
+     */
+    void isPossibleFormSecondWord(String word1, String word2) {
+
+        boolean ispossible = true;
+        OUT:
+        for (int i = 0; i < word2.length(); i++) {
+
+
+            if (!word1.contains(word2.substring(i, i + 1))) {
+                ispossible = false;
+                break OUT;
+            }
+        }
+        if (ispossible) {
+            System.out.println("possible");
+        } else {
+            System.out.println("not possible");
+        }
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("hello");
-
+        String s2 = "aapart";
+        String s1 = "apppart";
+        Lesson5_3_5 lesson5_3_5 = new Lesson5_3_5();
+        lesson5_3_5.isPossibleFormSecondWord(s1, s2);
     }
 }
