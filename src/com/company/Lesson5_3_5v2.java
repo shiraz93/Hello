51aa6f086d4a2d7ee0d07d0191d55552e7e43cee
Shiraz <shiraz007@list.ru>
2019-09-19 13:56:57 +0400

    lesson5.3.5.3versia2 orginal1

--- src/com/company/Lesson5_3_5v2.java
+++ src/com/company/Lesson5_3_5v2.java
@@ -4,39 +4,40 @@ public class Lesson5_3_5v2 {
     /**
      * print  it is possible form the second from the letters of the first
      * where each letters of the words quantity should be equal
+     *
      * @param word1
      * @param word2
      */
-    void isPossibleFormSecondWord(String word1, String word2){
-        boolean ispossible=true;
+    void isPossibleFormSecondWord(String word1, String word2) {
+        boolean ispossible = true;
         int firstcount;
         int secondcount;
-        if(word1.length()==word2.length()){
+        if (word1.length() == word2.length()) {
             OUT:
-            for (int i = 0; i <word2.length() ; i++) {
-                firstcount=0;
-                secondcount=0;
+            for (int i = 0; i < word2.length(); i++) {
+                firstcount = 0;
+                secondcount = 0;
                 for (int j = 0; j < word2.length(); j++) {
 
-                    if (word2.substring(i, i + 1).equals(word2.substring(j,j+1))){
+                    if (word2.substring(i, i + 1).equals(word2.substring(j, j + 1))) {
                         secondcount++;
                     }
-                    if (word2.substring(i, i + 1).equals(word1.substring(j,j+1))){
+                    if (word2.substring(i, i + 1).equals(word1.substring(j, j + 1))) {
                         firstcount++;
                     }
                 }
-                if(firstcount!=secondcount){
-                    ispossible=false;
+                if (firstcount != secondcount) {
+                    ispossible = false;
                     break OUT;
                 }
             }
 
-        }else {
-            ispossible=false;
+        } else {
+            ispossible = false;
         }
-        if(ispossible){
+        if (ispossible) {
             System.out.println("possible");
-        }else {
+        } else {
             System.out.println("not possible");
         }
     }
