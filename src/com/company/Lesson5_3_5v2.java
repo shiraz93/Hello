293544c67be7a8df9f2a64b0c039ac61aea95fff
Shiraz <shiraz007@list.ru>
2019-09-19 11:49:13 +0400

    lesson5.3.5.3versia2 orginal

--- src/com/company/Lesson5_3_5v2.java
+++ src/com/company/Lesson5_3_5v2.java
@@ -9,23 +9,23 @@ public class Lesson5_3_5v2 {
      */
     void isPossibleFormSecondWord(String word1, String word2){
         boolean ispossible=true;
-        int count1;
-        int count2;
+        int firstcount;
+        int secondcount;
         if(word1.length()==word2.length()){
             OUT:
             for (int i = 0; i <word2.length() ; i++) {
-                count1=0;
-                count2=0;
+                firstcount=0;
+                secondcount=0;
                 for (int j = 0; j < word2.length(); j++) {
 
                     if (word2.substring(i, i + 1).equals(word2.substring(j,j+1))){
-                        count2++;
+                        secondcount++;
                     }
                     if (word2.substring(i, i + 1).equals(word1.substring(j,j+1))){
-                        count1++;
+                        firstcount++;
                     }
                 }
-                if(count1!=count2){
+                if(firstcount!=secondcount){
                     ispossible=false;
                     break OUT;
                 }
