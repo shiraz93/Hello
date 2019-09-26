9b949268571055ef90f6621b91ca25a0f6e6ecc2
Shiraz <shiraz007@list.ru>
2019-09-26 19:55:10 +0400

    lesson6.3.4

---
+++ src/com/company/Lesson6_3_4.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Lesson6_3_4 {
+    /**
+     * this method get the largest segment which constis of odd numbers
+     * @param list
+     */
+    void getSegmentOdd(ArrayList<Integer> list) {
+        ArrayList<Integer> count = new ArrayList<>();
+        ArrayList<Integer> index = new ArrayList<>();
+        int quantity;
+
+        for (int i = 0; i < list.size(); i++) {
+
+            if (list.get(i) % 2 != 0) {
+                index.add(i);
+                quantity = 0;
+                OUT:
+                for (int j = i; j < list.size(); j++) {
+                    if (list.get(j) % 2 != 0) {
+                        quantity++;
+                        i++;
+                    } else {
+                        break OUT;
+                    }
+                }
+                count.add(quantity);
+            }
+        }
+        int maxCount = count.get(0);
+        int maxIndex = 0;
+        for (int i = 1; i < count.size(); i++) {
+            if (count.get(i) > maxCount) {
+                maxIndex = i;
+                maxCount = count.get(i);
+            }
+        }
+        ArrayList<Integer> arrayList = new ArrayList<>(count.get(maxIndex));
+        for (int i = index.get(maxIndex); i < index.get(maxIndex) + count.get(maxIndex); i++) {
+            arrayList.add(list.get(i));
+            System.out.println(list.get(i));
+        }
+
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,32 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(5);
+        list.add(7);
+        list.add(9);
+        list.add(11);
+        list.add(8);
+        list.add(1);
+        list.add(3);
+        list.add(16);
+        list.add(13);
+        list.add(19);
+        list.add(21);
+        list.add(27);
+        list.add(31);
+        Lesson6_3_4 lesson6_3_4=new Lesson6_3_4();
+        lesson6_3_4.getSegmentOdd(list);
 
-        System.out.println("hello");
 
     }
 }
