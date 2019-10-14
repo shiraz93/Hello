7dc4e84bef7f33dde248afe65c49fc52f1d3a3e4
Shiraz <shiraz007@list.ru>
2019-10-14 13:06:53 +0400

    lesson3.1.4 orginal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,12 @@ public class Main {
         String dir = "NESW";
         String direction = null;
         Scanner scanner = new Scanner(System.in);
-        LessonThreeLevelOneExcersiceOne threeLevelOneExcersiceOne = new LessonThreeLevelOneExcersiceOne();
+        LessonThreeLevelOneExcersiceFour threeLevelOneExcersiceFour = new LessonThreeLevelOneExcersiceFour();
         System.out.println("Write initial direction Robot <N>-North <E>-East <S>-South <W>-West ");
         do {
             direction = scanner.next();
             if (dir.contains(direction.toUpperCase())) {
-                threeLevelOneExcersiceOne.showDirectionRobotAfterPushButton(direction);
+                threeLevelOneExcersiceFour.showDirectionRobotAfterPushButton(direction);
             } else if (!direction.equals("Q")) {
                 System.out.println("you write mistake input " +
                         "\n Write initial direction Robot <N>-North <E>-East <S>-South <W>-West " +
