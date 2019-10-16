ee1a0d93b95a6f2e5f26a4adbde8ef3b46a40190
Shiraz <shiraz007@list.ru>
2019-10-16 17:39:05 +0400

    lesson 9.3.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,33 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello");
-
+        Mylist<Integer> mylist = new MyArrayList<>();
+        mylist.add(1);
+        mylist.add(2);
+        mylist.add(3);
+        mylist.add(4);
+        mylist.add(5);
+        mylist.add(6);
+        mylist.add(7);
+        mylist.add(8);
+        mylist.add(9);
+        mylist.add(10);
+        mylist.add(11);
+        mylist.add(12);
+        mylist.add(13);
+        mylist.add(14);
+        mylist.add(15);
+        mylist.remove(0);
+        
+        for (int i = 0; i < mylist.size(); i++) {
+            System.out.println(mylist.get(i));
+        }
     }
 }
