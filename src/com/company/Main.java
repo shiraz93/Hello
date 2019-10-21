6e6d48e1a4a30436cf8bcea573c8ac48f6f297c8
Shiraz <shiraz007@list.ru>
2019-10-21 13:01:33 +0400

    PracticallessonEx1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,21 @@
 package com.company;
 
+import com.sun.jdi.Value;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        PracticalLessonExcersiceOne excersiceOne = new PracticalLessonExcersiceOne();
+        System.out.println(excersiceOne.sum(5, 10));
+        System.out.println(excersiceOne.sum(4, 12));
+
 
     }
+
+
 }
---
+++ src/com/company/PracticalLessonExcersiceOne.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class PracticalLessonExcersiceOne {
+    /**
+     * return sum of x and y
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    int sum(int x, int y) {
+        return x + y;
+    }
+
+}
