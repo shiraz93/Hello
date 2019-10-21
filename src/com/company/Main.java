775d7126269d65d03fd29a61ac4b77cf59afc8c6
Shiraz <shiraz007@list.ru>
2019-10-21 15:42:50 +0400

    PracticallessonEx10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("hello");
+        int first[] = {1, 12, 45, 4, 54, 45, 4, 4, 1, 1};
+        PracticalLessonExerciseTen exerciseTen = new PracticalLessonExerciseTen();
+        System.out.println(exerciseTen.getCountElement(first));
+        int second[] = {45, 1, 2, 41, 584, 7};
+        System.out.println(exerciseTen.getCountElement(second));
 
     }
 }
---
+++ src/com/company/PracticalLessonExerciseTen.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class PracticalLessonExerciseTen {
+    /**
+     * return count of element array
+     *
+     * @param array
+     * @return
+     */
+    int getCountElement(int[] array) {
+        return array.length;
+    }
+}
